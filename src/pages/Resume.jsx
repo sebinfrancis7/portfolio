fbdb144699b888a35fd10ba0b20c5fa7d023a41c
sebinfrancis7 <sebin.k.francis07@gmail.com>
2021-12-09 17:12:33 +0530

    added meta tags

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -8,7 +8,7 @@ function Resume() {
             <div className=" inner-content">
                 <p className="heading-text text-center" ><u>ＲＥＳＵＭＥ </u><a href={resumeFile} download > <i className="fas fa-lg fa-file-download icon"> </i></a></p>
                 <div className="d-flex justify-content-center resume-box" style={{ height: '90%' }}>
-                    <iframe title="resume" src="https://drive.google.com/file/d/1EBnSpX1-CvzR0pXohMOnQ5PRhwUuX8WD/preview" allow="autoplay"
+                    <iframe title="resume" loading="lazy" src="https://drive.google.com/file/d/1EBnSpX1-CvzR0pXohMOnQ5PRhwUuX8WD/preview" allow="autoplay"
                         style={{ width: '80%', height: '90%', padding: ".5rem 2rem 2rem 2rem", }} frameborder="0"></iframe>
                 </div>
             </div>
