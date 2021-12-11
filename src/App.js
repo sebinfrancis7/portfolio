a32bcfca3f3a6392b26d8fbe9ebda352bbb13bee
sebinfrancis7 <sebin.k.francis07@gmail.com>
2021-12-11 11:16:52 +0530

    added scroll progress bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import Zoom from 'react-reveal/Zoom';
 import Jump from 'react-reveal/Jump';
 
 import LoadingScreen from 'react-loading-screen';
 import Navbar from "./components/Navbar/Navbar.jsx";
 import HomePage from './pages/HomePage.jsx';
-import AboutMe from './pages/AboutMe.jsx';
+// import AboutMe from './pages/AboutMe.jsx';
 import Skills from "./pages/Skills.jsx";
 import Contact from "./pages/Contact.jsx";
-import Resume from "./pages/Resume.jsx";
+// import Resume from "./pages/Resume.jsx";
 import Projects from "./pages/Projects.jsx";
 import Footer from "./components/Footer.jsx";
+
+const AboutMe = React.lazy(() => import('./pages/AboutMe.jsx'));
+const Resume = React.lazy(() => import('./pages/Resume.jsx'));
+
 
 function App() {
 
   const [loading, setLoading] = useState(true);
+  const [scroll, setScroll] = useState(0);
+
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 1500);
   }, []);
+
+  useEffect(() => {
+
+    let progressBarHandler = () => {
+
+      const totalScroll = document.documentElement.scrollTop;
+      const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const scroll = `${totalScroll / windowHeight}`;
+
+      setScroll(scroll);
+    }
+
+    window.addEventListener("scroll", progressBarHandler);
+
+    return () => window.removeEventListener("scroll", progressBarHandler);
+  });
 
   return (
     <div className="App">
@@ -35,18 +57,25 @@ function App() {
           :
           <div>
             <Navbar />
+            <div id="progressBarContainer">
+              <div id="progressBar" style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll}` }} />
+            </div>
             <Zoom>
               <HomePage />
             </Zoom>
-            <Zoom>
-              <AboutMe />
-            </Zoom>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Zoom>
+                <AboutMe />
+              </Zoom>
+            </Suspense>
             {/* <Zoom>
               <Skills />
             </Zoom> */}
-            <Zoom>
-              <Resume />
-            </Zoom>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Zoom>
+                <Resume />
+              </Zoom>
+            </Suspense>
             {/* <Zoom>
               <Projects />
             </Zoom> */}
