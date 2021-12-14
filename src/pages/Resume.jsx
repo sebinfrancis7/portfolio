3b8344f0801bfa99c964cab648acc1b194149fcb
sebinfrancis7 <sebin.k.francis07@gmail.com>
2021-12-14 10:39:47 +0530

    minor fixes

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,13 +3,19 @@ import resumeFile from '../assets/SEBIN FRANCIS KANNAMPUZHA - Resume.pdf'
 
 function Resume() {
 
+    const iframeStyle = {
+        width: '80%',
+        height: '90%',
+        padding: ".5rem 2rem 2rem 2rem"
+    }
+
     return (
         <div id="resume" className="container outer-content">
             <div className=" inner-content">
                 <p className="heading-text text-center" ><u>ＲＥＳＵＭＥ </u><a href={resumeFile} download > <i className="fas fa-lg fa-file-download icon"> </i></a></p>
                 <div className="d-flex justify-content-center resume-box" style={{ height: '90%' }}>
-                    <iframe title="resume" loading="lazy" src="https://drive.google.com/file/d/1EBnSpX1-CvzR0pXohMOnQ5PRhwUuX8WD/preview" allow="autoplay"
-                        style={{ width: '80%', height: '90%', padding: ".5rem 2rem 2rem 2rem", }} frameBorder="0"></iframe>
+                    <iframe title="resume" className='main-resume' loading="lazy" src="https://drive.google.com/file/d/1EBnSpX1-CvzR0pXohMOnQ5PRhwUuX8WD/preview" allow="autoplay"
+                        frameBorder="0" style={iframeStyle}></iframe>
                 </div>
             </div>
         </div >
